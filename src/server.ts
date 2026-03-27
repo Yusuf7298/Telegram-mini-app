5de48a23661b7deaf381919c6b22fa3e9163ae32
Yusuf7298 <ym47484988@gmail.com>
2026-03-27 22:40:38 +0300

    fix dependency conflict for deploymentu

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ const corsOptions: cors.CorsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
